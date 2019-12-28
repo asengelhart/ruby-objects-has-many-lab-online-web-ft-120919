19971c3bac980c13cb85cbc3407c3ca5a82a6585
Alex Engelhart <asengelhart@yahoo.com>
2019-12-28 00:11:40 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,1 +1,19 @@
 def Post
+
+  attr_accessor :title, :author 
+  @@all = [] 
+  
+  def initialize(title)
+    @title = tile
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def author_name
+    author ? author.name : nil 
+  end 
+  
+end 
