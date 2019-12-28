9ac4e1440e434198b544f74495daf77d80af63a5
Alex Engelhart <asengelhart@yahoo.com>
2019-12-28 00:03:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Artist 
   attr_accessor :name
   
@@ -6,6 +7,7 @@ class Artist
   end
   
   def songs
+    binding.pry 
     Songs.all.select{|song| song.artist == self}
   end 
 end 
